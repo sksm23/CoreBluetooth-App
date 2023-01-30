cd68b23aa52c112889204fc20f33922a3da14754
Sunil Kumar <sunil.sm@1mg.com>
2023-01-30 17:16:49 +0530

    Initial Commit

---
+++ CoreBluetooth-App/CoreBluetooth_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBluetooth_AppApp.swift
+//  CoreBluetooth-App
+//
+//  Created by Sunil Kumar on 30/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreBluetooth_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
